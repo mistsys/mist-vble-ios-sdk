e4c8274a4abbe68716f795e0c59bccf636cc8cd6
Ajay Gantayet <ajay.gantayet@mistsys.com>
2019-07-09 19:58:54 +0530

    SDK 1.5.47 Framework.

---
+++ Frameworks/1.5.47/MistSDK.framework/Headers/UIColor+Mist.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Mist.h
+//  Mist
+//
+//  Created by Cuong Ta on 7/6/15.
+//  Copyright (c) 2015 Mist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Mist)
+
++(instancetype)colorWithHexString:(NSString *)hexString;
+
+@end
