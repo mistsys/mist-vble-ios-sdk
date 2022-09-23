6b253d8d0cb5ff9d60dbf6bcfe136724ff26ed66
Cuong Ta <cuongchanta@gmail.com>
2022-09-23 14:04:39 -0700

    sdk - v1.5.291

---
+++ Frameworks/1.5.291/MistSDK.framework/Headers/UIImageView+Mist.h
@@ -0,0 +1,15 @@
+//
+//  UIImageView+Mist.h
+//  Mist
+//
+//  Created by Cuong Ta on 7/7/15.
+//  Copyright (c) 2015 Mist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (Mist)
+
+//-(void)pinAtRelativeLocation:(CGPoint)point atScaleFactor:(double)scaleFactor;
+
+@end
